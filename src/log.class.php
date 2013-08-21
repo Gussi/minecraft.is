6794a988243590ec7c448dc3674f50e11a19f65c
Gunnsteinn Þórisson <gussi@gussi.is>
2013-08-21 22:28:45 +0000

    Update log.class.php
    Gæti virkað

--- src/log.class.php
+++ src/log.class.php
@@ -2,8 +2,19 @@
 
 class log {
     private $log;
+    public static $instance;
+    
     public function __construct($file){
         $this->log = fopen($file, "a");
+        self::instance = $this;
+    }
+    
+    static public function info($msg) {
+        self::instance->write("INFO : " . $msg);
+    }
+    
+    static public function error($msg) {
+        self::instance->write("ERROR : " . $msg);
     }
 
     public function write($entry){
